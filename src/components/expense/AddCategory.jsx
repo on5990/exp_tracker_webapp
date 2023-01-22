29cd6b7aae4d31f63730745c96c77ad47e1a08e5
Oscar NM <on5990@gmail.com>
2023-01-22 03:49:47 -0300

    Fix New category button

--- src/components/expense/AddCategory.jsx
+++ src/components/expense/AddCategory.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 import { useState } from "react";
 
 function AddCategory() {
   const [showForm, setShowForm] = useState(false);
   const [category, setCategory] = useState("");
-  function handleClick() {
+  const ref = useRef(null);
+  function detectClickOutside(ref, func) {
+    function handleClick(event) {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      func(event);
+    }
+    useEffect(() => {
+      document.addEventListener("mousedown", handleClick);
+      document.addEventListener("touchstart", handleClick);
+      return () => {
+        document.removeEventListener("mousedown", handleClick);
+        document.removeEventListener("touchstart", handleClick);
+      };
+    }, [ref, func]);
+  }
+  function openInput() {
     setShowForm(true);
+  }
+  function closeInput() {
+    setShowForm(false);
+    setCategory("");
   }
   function handleSubmit() {
     if (category !== "") {
@@ -13,15 +36,16 @@ function AddCategory() {
       setCategory("");
     }
   }
+  detectClickOutside(ref, closeInput);
   return (
     <>
       {!showForm && (
-        <button type="button" className="newCategory" onClick={handleClick}>
+        <button type="button" className="newCategory" onClick={openInput}>
           <p>+ Nueva</p>
         </button>
       )}
       {showForm && (
-        <div className="categoryForm">
+        <div className="categoryForm" ref={ref}>
           <input
             type="text"
             value={category}
