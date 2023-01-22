1217ea8ec4ef128cde646484d1cc4891e718d472
Oscar NM <on5990@gmail.com>
2023-01-22 04:35:56 -0300

    fix

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -27,8 +27,13 @@ function Header() {
                 Gráficos
               </Link>
             </li>
+            {/* <li></li> */}
           </ul>
         </nav>
+        <div className="userSection">
+          <p>Oscar Navarro Mondaca</p>
+          <button>Salir</button>
+        </div>
       </header>
     </>
   );
